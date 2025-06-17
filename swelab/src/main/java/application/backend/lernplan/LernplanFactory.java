3778608f316d14a936ad41a3e1583c4a2adc55bd
Tim Schröder <tim.schroeder@hadiko.de>
2025-06-17 16:50:53 +0200

    fix again

--- swelab/src/main/java/application/backend/lernplan/LernplanFactory.java
+++ swelab/src/main/java/application/backend/lernplan/LernplanFactory.java
@@ -3,6 +3,6 @@ package application.backend.lernplan;
 import application.backend.lernplan.port.Lernplan;
 
 public interface LernplanFactory {
-    LernplanFactory FACTORY = new LernplanFactory(); // static access to the facade
+    LernplanFactory FACTORY = new LernplanFacade(); // static access to the facade
     Lernplan Lernplan(); // return the implementation of the interface Xy
 }
